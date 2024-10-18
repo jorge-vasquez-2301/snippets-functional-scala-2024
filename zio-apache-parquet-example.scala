a412b12475e2d405602aff7084a3059adef3345c
Jorge Vasquez <jorge.vasquez0123@proton.me>
2024-10-18 12:08:13 -0400

    Add parquet example

---
+++ zio-apache-parquet-example.scala
@@ -0,0 +1,113 @@
+//> using jvm graalvm-java11:22.3.3
+//> using scala 3.5.1
+//> using dep co.fs2::fs2-core:3.11.0
+//> using dep dev.zio::zio:2.1.11
+//> using dep dev.zio::zio-streams:2.1.11
+//> using dep dev.zio::zio-interop-cats:23.1.0.3
+//> using dep info.fingo::spata:3.2.1
+//> using dep dev.zio::zio-schema:1.5.0
+//> using dep dev.zio::zio-schema-derivation:1.5.0
+//> using dep me.mnedokushev::zio-apache-parquet-core:0.1.3
+
+import zio.schema.*
+import me.mnedokushev.zio.apache.parquet.core.codec.*
+import me.mnedokushev.zio.apache.parquet.core.hadoop.{ ParquetReader, ParquetWriter, Path }
+import zio.*
+import zio.interop.catz.*
+import zio.stream.interop.fs2z.*
+
+import java.nio.file.Files
+import java.nio.file.Paths
+import info.fingo.spata.io.Reader
+import info.fingo.spata.{ CSVParser, Record }
+import zio.stream.*
+import info.fingo.spata.CSVRenderer
+import info.fingo.spata.io.Writer
+import me.mnedokushev.zio.apache.parquet.core.filter.syntax.*
+import me.mnedokushev.zio.apache.parquet.core.filter.*
+
+object ZIOApacheParquetExample extends ZIOAppDefault:
+  type Eff[A] = RIO[Scope, A]
+
+  final case class Element(element: String, symbol: String, meltingTemp: Double, boilingTemp: Double):
+    self =>
+
+    def updateTemps(f: Double => Double) =
+      self.copy(meltingTemp = f(self.meltingTemp), boilingTemp = f(self.boilingTemp))
+
+  object Element:
+    given Schema.CaseClass4.WithFields[
+      "element",
+      "symbol",
+      "meltingTemp",
+      "boilingTemp",
+      String,
+      String,
+      Double,
+      Double,
+      Element
+    ] = DeriveSchema.gen[Element]
+
+    given SchemaEncoder[Element] = Derive.derive[SchemaEncoder, Element](SchemaEncoderDeriver.default)
+    given ValueEncoder[Element]  = Derive.derive[ValueEncoder, Element](ValueEncoderDeriver.default)
+    given ValueDecoder[Element]  = Derive.derive[ValueDecoder, Element](ValueDecoderDeriver.default)
+    given TypeTag[Element]       = Derive.derive[TypeTag, Element](TypeTagDeriver.default)
+
+    val (element, symbol, meltingTemp, boilingTemp) = Filter[Element].columns
+
+  extension [A](a: A)
+    def toRecord(using schema: Schema.Record[A]) =
+      Record.fromPairs(schema.fields.map(field => field.fieldName -> field.get(a).toString)*)
+
+  val elementsFahrenheitCSVFile      = Paths.get("testdata/elements-fahrenheit.csv")
+  val elementsCelsiusParquetFile     = Path(Paths.get("testdata/elements-celsius.parquet"))
+  val elementsCelsiusCSVFile         = Paths.get("testdata/elements-celsius.csv")
+  val elementsCelsiusFilteredCSVFile = Paths.get("testdata/elements-celsius-filtered.csv")
+
+  val csvParser: fs2.Pipe[Eff, Char, Record] =
+    CSVParser.config
+      .mapHeader(Map("melting temperature [F]" -> "meltingTemp", "boiling temperature [F]" -> "boilingTemp"))
+      .parser[Eff]
+      .parse
+
+  val csvRenderer: fs2.Pipe[Eff, Record, Char] =
+    CSVRenderer.config
+      .fieldDelimiter(';')
+      .mapHeader(Map("meltingTemp" -> "melting temperature [C]", "boilingTemp" -> "boiling temperature [C]"))
+      .renderer[Eff]
+      .render
+
+  val processor: RIO[ParquetWriter[Element] & ParquetReader[Element], Unit] =
+    def fahrenheitToCelsius(f: Double): Double = (f - 32.0) * (5.0 / 9.0)
+
+    def writeToCsv(stream: ZStream[Scope, Throwable, Element], path: java.nio.file.Path) =
+      stream
+        .map(_.toRecord)
+        .toFs2Stream
+        .through(csvRenderer)
+        .through(Writer[Eff].write(path))
+        .compile
+        .drain
+
+    val elementsStream: ZStream[Scope, Throwable, Element] =
+      Reader[Eff]
+        .read(elementsFahrenheitCSVFile)
+        .through(csvParser)
+        .toZStream()
+        .mapZIO(record => ZIO.fromEither(record.to[Element]))
+        .map(_.updateTemps(fahrenheitToCelsius))
+
+    ZIO.scoped {
+      for
+        _                      <- ZIO.serviceWithZIO[ParquetWriter[Element]](_.writeStream(elementsCelsiusParquetFile, elementsStream))
+        allElementsStream      <- ZIO.serviceWith[ParquetReader[Element]](_.readStream(elementsCelsiusParquetFile))
+        _                      <- writeToCsv(allElementsStream, elementsCelsiusCSVFile)
+        filteredElementsStream <-
+          ZIO.serviceWith[ParquetReader[Element]] {
+            _.readStreamFiltered(elementsCelsiusParquetFile, filter(Element.element =!= "hydrogen"))
+          }
+        _                      <- writeToCsv(filteredElementsStream, elementsCelsiusFilteredCSVFile)
+      yield ()
+    }
+
+  override def run = processor.provide(ParquetWriter.configured[Element](), ParquetReader.configured[Element]())
