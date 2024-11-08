e9abe31c78ec4fbd8b3306e02ea225b029a59c9f
Jorge Vasquez <jorge.vasquez0123@proton.me>
2024-11-08 11:51:35 -0400

    Improvements

--- spata-example.scala
+++ spata-example.scala
@@ -1,7 +1,7 @@
 //> using jvm graalvm-java23:23.0.0
 //> using scala 3.5.2
-//> using dep dev.zio::zio-interop-cats:23.1.0.3
 //> using dep info.fingo::spata:3.2.1
+//> using dep dev.zio::zio-interop-cats:23.1.0.3
 //> using dep dev.zio::zio-schema:1.5.0
 //> using dep dev.zio::zio-schema-derivation:1.5.0
 
@@ -27,22 +27,34 @@ object SpataExample extends ZIOAppDefault:
     def toRecord(using schema: Schema.Record[A]) =
       Record.fromPairs(schema.fields.map(field => field.fieldName -> field.get(a).toString)*)
 
-  val csvParserWithDefaults: fs2.Pipe[Task, Char, Record] = CSVParser[Task].parse
+  // Default CSVParser, according to RFC 4180
+  // Delimiter: Comma (,)
+  // Quote character: Double quote (")
+  // The first line of the file is considered the header row
+  // Headers will be mapped to case class fields
+  val csvParserWithDefaults: CSVParser[Task] = CSVParser[Task]
 
-  val csvParser: fs2.Pipe[Task, Char, Record] =
+  val csvParser: CSVParser[Task] =
     CSVParser.config
       .mapHeader(Map("melting temperature [F]" -> "meltingTemp", "boiling temperature [F]" -> "boilingTemp"))
       .parser[Task]
-      .parse
 
-  val csvRendererWithDefaults: fs2.Pipe[Task, Record, Char] = CSVRenderer[Task].render
+  val csvParserPipe: fs2.Pipe[Task, Char, Record] = csvParser.parse
 
-  val csvRenderer: fs2.Pipe[Task, Record, Char] =
+  // Default CSVRenderer, according to RFC 4180
+  // Delimiter: Comma (,)
+  // Quote character: Double quote (")
+  // The rendered CSV will contain a header row
+  // Case class fields will be mapped to headers
+  val csvRendererWithDefaults: CSVRenderer[Task] = CSVRenderer[Task]
+
+  val csvRenderer: CSVRenderer[Task] =
     CSVRenderer.config
       .fieldDelimiter(';')
       .mapHeader(Map("meltingTemp" -> "melting temperature [C]", "boilingTemp" -> "boiling temperature [C]"))
       .renderer[Task]
-      .render
+
+  val csvRendererPipe: fs2.Pipe[Task, Record, Char] = csvRenderer.render
 
   val fahrenheitCSV = "testdata/elements-fahrenheit.csv"
   val celsiusCSV    = "testdata/elements-celsius.csv"
@@ -52,13 +64,15 @@ object SpataExample extends ZIOAppDefault:
 
     Reader[Task]
       .read(Paths.get(fahrenheitCSV))
-      .through(csvParser)
+      .through(csvParserPipe)
       .toZStream()
       .mapZIO(record => ZIO.fromEither(record.to[Element]))
-      .map(_.updateTemps(fahrenheitToCelsius).toRecord)
+      .map(_.updateTemps(fahrenheitToCelsius))
+      .map(_.toRecord) // toRecord is actually an extension method
       .toFs2Stream
-      .through(csvRenderer)
+      .through(csvRendererPipe)
       .through(Writer[Task].write(Paths.get(celsiusCSV)))
 
   val run =
-    ZIO.log(s"Processing $fahrenheitCSV") *> processor.compile.drain
+    ZIO.log(s"Processing $fahrenheitCSV")
+      *> processor.compile.drain
--- zio-apache-parquet-example.scala
+++ zio-apache-parquet-example.scala
@@ -1,14 +1,15 @@
 //> using jvm graalvm-java11:22.3.3
 //> using scala 3.5.2
-//> using dep dev.zio::zio-interop-cats:23.1.0.3
+//> using dep me.mnedokushev::zio-apache-parquet-core:0.1.4
 //> using dep info.fingo::spata:3.2.1
+//> using dep dev.zio::zio-interop-cats:23.1.0.3
 //> using dep dev.zio::zio-schema:1.5.0
 //> using dep dev.zio::zio-schema-derivation:1.5.0
-//> using dep me.mnedokushev::zio-apache-parquet-core:0.1.3
 
 import zio.schema.*
 import me.mnedokushev.zio.apache.parquet.core.codec.*
 import me.mnedokushev.zio.apache.parquet.core.hadoop.{ ParquetReader, ParquetWriter, Path }
+import org.apache.parquet.hadoop.*
 import zio.*
 import zio.interop.catz.*
 import zio.stream.interop.fs2z.*
@@ -45,21 +46,21 @@ object ZIOApacheParquetExample extends ZIOAppDefault:
       Element
     ] = DeriveSchema.gen[Element]
 
+    // SchemaEncoder is used to generate the corresponding Parquet Schema when writing files
     given SchemaEncoder[Element] = Derive.derive[SchemaEncoder, Element](SchemaEncoderDeriver.default)
-    given ValueEncoder[Element]  = Derive.derive[ValueEncoder, Element](ValueEncoderDeriver.default)
-    given ValueDecoder[Element]  = Derive.derive[ValueDecoder, Element](ValueDecoderDeriver.default)
-    given TypeTag[Element]       = Derive.derive[TypeTag, Element](TypeTagDeriver.default)
 
+    // Element => Value
+    given ValueEncoder[Element] = Derive.derive[ValueEncoder, Element](ValueEncoderDeriver.default)
+
+    // Value => Element
+    given ValueDecoder[Element] = Derive.derive[ValueDecoder, Element](ValueDecoderDeriver.default)
+
+    given TypeTag[Element]                          = Derive.derive[TypeTag, Element](TypeTagDeriver.default)
     val (element, symbol, meltingTemp, boilingTemp) = Filter[Element].columns
 
   extension [A](a: A)
     def toRecord(using schema: Schema.Record[A]) =
       Record.fromPairs(schema.fields.map(field => field.fieldName -> field.get(a).toString)*)
-
-  val elementsFahrenheitCSVFile      = Paths.get("testdata/elements-fahrenheit.csv")
-  val elementsCelsiusParquetFile     = Path(Paths.get("testdata/elements-celsius.parquet"))
-  val elementsCelsiusCSVFile         = Paths.get("testdata/elements-celsius.csv")
-  val elementsCelsiusFilteredCSVFile = Paths.get("testdata/elements-celsius-filtered.csv")
 
   val csvParser: fs2.Pipe[Eff, Char, Record] =
     CSVParser.config
@@ -73,6 +74,10 @@ object ZIOApacheParquetExample extends ZIOAppDefault:
       .mapHeader(Map("meltingTemp" -> "melting temperature [C]", "boilingTemp" -> "boiling temperature [C]"))
       .renderer[Eff]
       .render
+
+  val elementsFahrenheitCSVFile      = Paths.get("testdata/elements-fahrenheit.csv")
+  val elementsCelsiusParquetFile     = Path(Paths.get("testdata/elements-celsius.parquet"))
+  val elementsCelsiusFilteredCSVFile = Paths.get("testdata/elements-celsius-filtered.csv")
 
   val processor: RIO[ParquetWriter[Element] & ParquetReader[Element], Unit] =
     def fahrenheitToCelsius(f: Double): Double = (f - 32.0) * (5.0 / 9.0)
@@ -96,11 +101,13 @@ object ZIOApacheParquetExample extends ZIOAppDefault:
 
     ZIO.scoped {
       for
-        _                      <- ZIO.log(s"Writing all elements to $elementsCelsiusParquetFile")
+        _                      <- ZIO.log(s"Writing all elements to $elementsCelsiusParquetFile, records' schema will be:")
+        _                      <- ZIO.log {
+                                    summon[SchemaEncoder[Element]].encode(summon[Schema[Element]], "element", optional = false).toString
+                                  }
         _                      <- ZIO.serviceWithZIO[ParquetWriter[Element]](_.writeStream(elementsCelsiusParquetFile, elementsStream))
         _                      <- ZIO.log(s"Reading all elements from $elementsCelsiusParquetFile")
         allElementsStream      <- ZIO.serviceWith[ParquetReader[Element]](_.readStream(elementsCelsiusParquetFile))
-        _                      <- writeToCsv(allElementsStream, elementsCelsiusCSVFile)
         _                      <- ZIO.log(s"Filtering elements from $elementsCelsiusParquetFile")
         filteredElementsStream <-
           ZIO.serviceWith[ParquetReader[Element]] {
@@ -112,6 +119,6 @@ object ZIOApacheParquetExample extends ZIOAppDefault:
     }
 
   override def run = ZIO.log(s"Processing $elementsFahrenheitCSVFile") *> processor.provide(
-    ParquetWriter.configured[Element](),
+    ParquetWriter.configured[Element](writeMode = ParquetFileWriter.Mode.OVERWRITE),
     ParquetReader.configured[Element]()
   )
